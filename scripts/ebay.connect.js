52ed09c8f86c3eba72d1b214c2f148743800d82d
JD <44527761+jaydiare@users.noreply.github.com>
2026-02-26 16:02:50 -0500

    Create ebay.connect.js

---
+++ scripts/ebay.connect.js
@@ -0,0 +1,20 @@
+import crypto from "crypto";
+import { EBAY_AUTH_URL } from "../utils/ebay.config.js";
+
+export const ebayConnect = (req, res) => {
+  const state = crypto.randomBytes(16).toString("hex");
+
+  res.cookie("ebay_oauth_state", state, {
+    httpOnly: true,
+    sameSite: "lax",
+  });
+
+  const url =
+    `${EBAY_AUTH_URL}?client_id=${encodeURIComponent(process.env.EBAY_CLIENT_ID)}` +
+    `&response_type=code` +
+    `&redirect_uri=${encodeURIComponent(process.env.EBAY_REDIRECT_URI)}` +
+    `&scope=${encodeURIComponent(process.env.EBAY_SCOPES)}` +
+    `&state=${encodeURIComponent(state)}`;
+
+  res.redirect(url);
+};
