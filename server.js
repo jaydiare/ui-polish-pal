6ec389958765742baa2005720d9c10725695e4f8
JD <44527761+jaydiare@users.noreply.github.com>
2026-02-26 19:03:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 
-import { ebayConnect } from "./server/routes/ebay.connect.js";
-import { ebayCallback } from "./server/routes/ebay.callback.js";
+import { ebayConnect } from "./scripts/ebay.connect.js";
+import { ebayCallback } from "./scripts/ebay.callback.js";
 
 dotenv.config();
 
@@ -13,6 +13,4 @@ app.use(cookieParser());
 app.get("/api/ebay/connect", ebayConnect);
 app.get("/api/ebay/callback", ebayCallback);
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+app.listen(3000, () => console.log("Server running on port 3000"));
